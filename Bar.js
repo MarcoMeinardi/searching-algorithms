da11c5bf5ba6d23d101cfcf9e08401fd40f337e0
MarcoMeinardi <marcomeinardi01@gmail.com>
2020-08-28 19:49:36 +0200

    css fixes

--- Bar.js
+++ Bar.js
@@ -2,19 +2,22 @@ class Bar {
 	constructor (val, ind) {
 		this.val = val;
 		this.height = Math.floor (this.val * 500 / n);
-		this.width = Math.floor (1000 / n);
+		this.width = Math.floor (1000 / n) - 1;
+		this.left = ind * (this.width + 1) + 1;
+		this.top = 501 - this.height;
+
 		this.el = document.createElement ("div");
 		this.el.classList = "bar";
 		this.el.setAttribute ("style", 
-			"width: " + (this.width - 1) + "px; " + 
+			"width: " + this.width + "px; " + 
 			"height: " + this.height + "px; " +
-			"left: " + ind * this.width + "px; " + 
-			"top: " + (500 - this.height) + "px;"
+			"left: " + this.left + "px; " + 
+			"top: " + this.top + "px;"
 		);
 	}
 
 	async move (to, shuffle = false) {
-		this.el.style.left = to * this.width + "px";
+		this.el.style.left = (to * (this.width + 1) + 1) + "px";
 		if (!shuffle) {
 			this.el.style.zIndex = 2;
 			this.el.style.backgroundColor = "rgb(255, 0, 0)";
@@ -31,7 +34,7 @@ class Bar {
 		this.el.style.zIndex = 2;
 		this.el.style.backgroundColor = "rgb(255, 0, 0)";
 		this.el.style.height = this.height + "px";
-		this.el.style.top = (500 - this.height) + "px";
+		this.el.style.top = (501 - this.height) + "px";
 		await new Promise (r => setTimeout (r, speed));
 		this.el.style.zIndex = 1;
 		this.el.style.backgroundColor = "rgb(255, 255, 255)";
